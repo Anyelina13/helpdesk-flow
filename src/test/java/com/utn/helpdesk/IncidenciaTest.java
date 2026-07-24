cc0393ea374686886b7b9d6b8332560df4f7c590
Anyelina Chacon <chaconange5@gmail.com>
2026-07-24 10:59:40 -0600

    test: agregar prueba que falla para titulo vacio

---
+++ src/test/java/com/utn/helpdesk/IncidenciaTest.java
@@ -0,0 +1,15 @@
+package com.utn.helpdesk;
+
+import com.utn.helpdesk.model.Incidencia;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncidenciaTest {
+
+    @Test
+    void tituloNoPuedeEstarVacio() {
+        assertThrows(IllegalArgumentException.class, () ->
+            new Incidencia("", "Descripcion valida con mas de 10 caracteres", null, null, null)
+        );
+    }
+}
