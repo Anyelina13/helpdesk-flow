831ce162c10a19c57a2038bbd98e72923e06c9ab
Anyelina Chacon <chaconange5@gmail.com>
2026-07-24 11:06:45 -0600

    test: agregar prueba que falla para prioridad critica

---
+++ src/test/java/com/utn/helpdesk/CalculadorPrioridadTest.java
@@ -0,0 +1,16 @@
+package com.utn.helpdesk;
+
+import com.utn.helpdesk.model.*;
+import com.utn.helpdesk.service.CalculadorPrioridad;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadorPrioridadTest {
+
+    private final CalculadorPrioridad calculador = new CalculadorPrioridad();
+
+    @Test
+    void impactoAltoUrgenciaAltaEsCritica() {
+        assertEquals(Prioridad.CRITICA, calculador.calcular(Impacto.ALTO, Urgencia.ALTA));
+    }
+}
